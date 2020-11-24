fb6a28dcc9919aaae6514614418772cb4ebff902
SOONMYEONGKWON <smkwon@neonexsoft.com>
2020-11-25 00:45:30 +0900

    BinaryNumbers

--- src/com/soonworld/dayofcode/BinaryNumbers.java
+++ src/com/soonworld/dayofcode/BinaryNumbers.java
@@ -7,6 +7,10 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
+/**
+ * 10진수 -> 2진수 변환 뒤 연속적인 1의 개수를 구하는 문제
+ * 이진수로 변환 뒤 0을 기준으로 짜를 시 연속적인 1의 개수씩 배열에 들어가게 됨
+ */
 public class BinaryNumbers {
 
     private static final Scanner scanner = new Scanner(System.in);
@@ -16,5 +20,15 @@ public class BinaryNumbers {
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
         scanner.close();
+
+        int len = 0;
+        String num = Integer.toString(n,2);
+        String[] cmpNum = num.split("0");
+
+        for(String str : cmpNum){
+            if(len < str.length()) len= str.length();
+        }
+
+        System.out.println(len);
     }
 }
