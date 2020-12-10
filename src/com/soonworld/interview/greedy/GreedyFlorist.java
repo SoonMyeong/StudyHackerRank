dfa6f211caef24374bf03570d48195bf92a3d695
SOONMYEONGKWON <smkwon@neonexsoft.com>
2020-12-10 17:06:43 +0900

    Greedy Florist

--- src/com/soonworld/interview/greedy/GreedyFlorist.java
+++ src/com/soonworld/interview/greedy/GreedyFlorist.java
@@ -20,14 +20,16 @@ public class GreedyFlorist {
     static int getMinimumCost(int k, int[] c) {
         Arrays.sort(c); //오름차순 진행
         int min = 0;
+        int count =0;
+        int val = 1;
+
+        //가독성 면에선 괜찮다고 생각함..
         //1. 손님 수와 꽃의 리스트 개수가 같을 땐 꽃 가격의 합을 모두 더하면 된다. (각자 한송이 씩만 사면 되니까)
         if(k==c.length){
             for(int price : c){
                 min += price;
             }
         }else if(k<c.length){ //2. 손님 수 보다 꽃의 리스트 개수가 더 많을 경우엔 꽃 리스트(가격 오름차순 기준)끝에서 부터 구매를 한다.
-            int count =0;
-            int val = 1;
             for(int i=c.length-1; i>=0; i--){
                 count++;
                 if(count<=k){
@@ -39,8 +41,6 @@ public class GreedyFlorist {
                 }
             }
         }else{ //3. 꽃의 리스트 개수 보다 손님 수가 더 많을 경우엔 꽃 리스트(가격 오름차순 기준)처음에서 부터 구매를 한다.
-            int count = 0;
-            int val = 1;
             for(int i=0; i<k; i++){
                 count++;
                 if(count<=c.length) {
